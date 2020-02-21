0b4cc92544e9a1da2f6f40117a584dccebc1603e
iamsap <robbymillsap@gmail.com>
2020-02-20 21:30:00 -0800

    Added build-pipeline.yaml

--- AzureDevOps-CI/Startup.cs
+++ AzureDevOps-CI/Startup.cs
@@ -46,3 +46,5 @@ namespace AzureDevOps_CI
         }
     }
 }
+
+sdfsdfksdjklh
---
+++ AzureDevOps-CI/build.bat
@@ -0,0 +1,4 @@
+dotnet restore
+dotnet build --configuration Debug
+dotnet test
+dotnet publish --configuration Release
